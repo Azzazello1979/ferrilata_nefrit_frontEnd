95c37ff29793bed1d76cba59071951c5a3eed6ad
Soma <soma.feigl@gmail.com>
2019-06-26 13:24:04 +0200

    restarted whole project with ang/material

--- Reddit/src/app/login/login.component.ts
+++ Reddit/src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,26 +6,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username: string;
-  password: string;
-  form: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  submitData() {  
-    this.username = this.form.get('UN').value;
-    this.password = this.form.get('PW').value;
-    // sendback
-    // succes:this.form.get('first').setValue('some value');
-    // this.form.get('first').setValue('some value');
-    // err(showerr)
-  }
-
+  constructor() { }
   ngOnInit() {
-    this.form = this.formBuilder.group({
-      UN: [null, Validators.required],
-      PW: [null, Validators.required],
-    });
   }
 
 }
