3f702a3afddd78bf7b5c0f92ab07ab768fc81fe7
szabolcsveer <futuremove@gmail.com>
2019-06-26 11:40:38 +0200

    feat(post-service.ts) add array of object with dummy data to test with

--- Reddit/src/app/components/posts/posts.component.ts
+++ Reddit/src/app/components/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Posts } from '../../posts.model';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
--- Reddit/src/app/services/post-service.service.ts
+++ Reddit/src/app/services/post-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { Posts } from '../posts.model';
 @Injectable({
   providedIn: 'root'
 })
 export class PostServiceService {
-
-  constructor() { }
+  posts: Posts[] = [
+    {
+      id: '5d132cf4237ea11478a7e58a',
+      title: 'title',
+      content: 'content',
+      channel: 'channel',
+      timestamp: 6706754409533014017
+    },
+    {
+      id: '5d132f91237ea11478a7e58c',
+      title: 'title',
+      content: 'content',
+      channel: 'channel',
+      timestamp: 6706756737405288449
+    },
+    {
+      id: '5d132f91237ea11478a7e58c',
+      title: 'title',
+      content: 'content',
+      channel: 'channel',
+      timestamp: 6706756737405288449
+    }
+  ];
+  constructor() {}
 }
