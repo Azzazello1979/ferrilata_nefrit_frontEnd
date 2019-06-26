ba4bd6825b22d164fc652f5afbe0a336bf889b59
szabolcsveer <futuremove@gmail.com>
2019-06-26 14:06:48 +0200

    add basic styling

--- Reddit/src/app/app.module.ts
+++ Reddit/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './components/posts/posts.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -12,8 +17,15 @@ import { PostsComponent } from './components/posts/posts.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Reddit/src/app/components/posts/posts.component.ts
+++ Reddit/src/app/components/posts/posts.component.ts
@@ -14,7 +14,6 @@ content: string;
 channel: string;
 timestamp: number;
 posts: Posts[] = [];
-counter: number = 0;
 
   constructor(private postservice: PostServiceService) { }
 
