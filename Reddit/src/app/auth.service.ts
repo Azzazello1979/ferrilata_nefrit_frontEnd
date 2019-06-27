0ede50d68357a1c30b7c5d331ebc64bb0eda2876
Soma <soma.feigl@gmail.com>
2019-06-27 15:56:19 +0200

    added sendData() WIP (to send to backend)

--- Reddit/src/app/auth.service.ts
+++ Reddit/src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+const jwt = require('jsonwebtoken');
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,15 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  sendData(username: string, password: string) {
+    let payload = {
+      user: username,
+      word: password
+    };
+    // send(token){
+    // if(response){return true /false }}
+    let token = jwt.sign(payload, 'secretKey');
+    return false
+  }
 }
