32ee3c0b9bc68503ffd15ace5e5b1b8f788b041b
szabolcsveer <futuremove@gmail.com>
2019-06-27 13:42:33 +0200

    fix(post-service) removed commented code

--- Reddit/src/app/services/post-service.service.ts
+++ Reddit/src/app/services/post-service.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Posts } from '../posts.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
@@ -7,29 +6,6 @@ import { HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class PostServiceService {
-  // posts: Posts[] = [
-  //   {
-  //     id: '5d132cf4237ea11478a7e58a',
-  //     title: 'title',
-  //     content: 'content',
-  //     channel: 'channel',
-  //     timestamp: 6706754409533014017
-  //   },
-  //   {
-  //     id: '5d132f91237ea11478a7e58c',
-  //     title: 'title',
-  //     content: 'content',
-  //     channel: 'channel',
-  //     timestamp: 6706756737405288449
-  //   },
-  //   {
-  //     id: '5d132f91237ea11478a7e58c',
-  //     title: 'title',
-  //     content: 'content',
-  //     channel: 'channel',
-  //     timestamp: 6706756737405288449
-  //   }
-  // ];
   constructor(private http: HttpClient) {}
 
   getPosts(): any {
