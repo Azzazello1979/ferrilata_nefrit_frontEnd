be0cc672cbee14be9c565b38d0c22df5a32b3a31
Soma <soma.feigl@gmail.com>
2019-06-27 15:40:02 +0200

    feat(submitData)  extended function  w else/router

--- Reddit/src/app/login/login.component.ts
+++ Reddit/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +9,30 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
+  valid: boolean = true;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   hasError(controlName: string, errorName: string) {
     return this.form.controls[controlName].hasError(errorName);
   }
 
-  // submitData() {
-  //   if (this.svc.sendData(this.form.get('username'), this.form.get('password')).response.statusCode == 200) {
-  //     this.svc.sendData(this.form.get('username'), this.form.get('password'));
-  //     this.router.navigate(['/forecast/:userid']);
-  //   } else
-  //     return false
-  // }
-
+  submitData() {//NO SERVICE YET
+    // if (this.authsvc.sendData(this.form.get('username').value, this.form.get('password').value).response.statusCode == 200) {
+    if (this.authsvc.sendData(this.form.get('username').value, this.form.get('password').value)) { //is something
+      this.authsvc.sendToken();
+      this.router.navigate(['/']);
+    } else {
+      // this.form.setValue({
+      //   ['username']: '',
+      //   ['password']: ''
+      // })
+      this.valid = false;
+      // setTimeout(() => {
+      //   this.valid = true;
+      // }, 5000)
+    }
+  }
   ngOnInit() {
     this.form = this.fb.group({
       username: ['', [Validators.required]],
