1fc82c2277fa3db932b3ae2f47c3cf0a3f6aa832
Soma <soma.feigl@gmail.com>
2019-06-28 11:38:36 +0200

    constructed http.post request added subject/obs

--- Reddit/src/app/auth.service.ts
+++ Reddit/src/app/auth.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
-const jwt = require('jsonwebtoken'); //FIXME
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
+// const jwt = require('jsonwebtoken'); //FIXME
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public username: BehaviorSubject<any> = new BehaviorSubject(1);
+  obs = this.username.asObservable();
+  public checker: Subject<boolean> = new Subject;
+  obs2 = this.checker.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  get user(): BehaviorSubject<string> {
+    return this.username
+  }
 
   sendData(username: string, password: string) {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    // let token = jwt.sign(payload, 'secretKey'); //FIXME
+    // headers = headers.append('Authorization', '');
     let payload = {
-      user: username,
-      word: password
+      username: username,
+      password: password
     };
-    // if(response.message=="Wrong username or password."){return false}
-    // else{}
-    let token = jwt.sign(payload, 'secretKey'); //FIXME
-    return true
+    this.http.post('http://localhost:3000', payload, { headers: headers }).subscribe(
+      res => {
+        this.username.next({ succes: true, username });
+        this.checker.next(true) 
+      },
+      err => {
+        this.checker.next(false) 
+      })
   }
-}
+};
