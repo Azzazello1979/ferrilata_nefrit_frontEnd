25910943c5d42403003989c84a2c1e24dde726d4
szabolcsveer <futuremove@gmail.com>
2019-07-03 09:03:31 +0200

    feat(dropdown) Please select and Default Value features working

--- Reddit/src/app/components/dropdown/dropdown.component.ts
+++ Reddit/src/app/components/dropdown/dropdown.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormBuilder,  FormControl, FormGroup, FormArray, NgModel, Validators, } from '@angular/forms';
 
+import { of } from 'rxjs';
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
-  @Input() entities: any;
-  @Input() displayableProperty: string;
-  @Input() defaultValue: number;
-  @Output() selectedEntity = new EventEmitter();
-  name = new FormControl('');
-  constructor() { }
+  // form: FormGroup;
+
+  entity: FormGroup;
+
+  @Input() private entities: any;
+  @Input() private displayableProperty: string;
+  @Input() private defaultValue: string;
+  @Output() selectedEntity: EventEmitter<any> = new EventEmitter();
+  constructor(private formBuilder: FormBuilder) {
+
+    }
+
 
   ngOnInit() {
+
+    this.entity = this.formBuilder.group({
+    entity: [null, Validators.required]
+    });
+    this.entity.get('entity').setValue(this.defaultValue);
+    console.log(this.defaultValue);
   }
 
 }
--- Reddit/src/app/components/dummy-dialog/dummy-dialog.component.ts
+++ Reddit/src/app/components/dummy-dialog/dummy-dialog.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogService } from 'src/app/services/dialog.service';
-
+import { DropdownComponent } from '../dropdown/dropdown.component';
 @Component({
   selector: 'app-dummy-dialog',
   templateUrl: './dummy-dialog.component.html',
   styleUrls: ['./dummy-dialog.component.css']
 })
 export class DummyDialogComponent implements OnInit {
+  displayableProperty: any = 'email';
+  entities: any = [
+    { name: 'Sally', age: 33, email: 'sally@gmail.com' },
+    { name: 'John', age: 23, email: 'john@gmail.com' },
+    { name: 'Art', age: 43, email: 'art@gmail.com' }
+  ];
 
-  constructor(private dialog: DialogService) { }
+  selectedValue: any;
+  defaultValue: any;
+  constructor(private dialog: DialogService) {}
 
   ngOnInit() {
-    this.dialog.openError('Unknown Error', 400);
+    this.dialog.openError('Unknown Error', 400);   
   }
-
 }
