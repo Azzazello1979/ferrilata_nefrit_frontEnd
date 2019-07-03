ff35ff7cf8a1a9637eceb5c92a9d6703eee2aa14
szabolcsveer <futuremove@gmail.com>
2019-07-03 15:28:18 +0200

    reorganized modules order

--- Reddit/src/app/app.module.ts
+++ Reddit/src/app/app.module.ts
@@ -3,19 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DummyDialogComponent } from './components/dummy-dialog/dummy-dialog.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
-import { HttpClientModule } from '@angular/common/http';
-import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { DummyDialogComponent } from './components/dummy-dialog/dummy-dialog.component';
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
+import { PostsComponent } from './components/posts/posts.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PostsComponent, ErrorDialogComponent, DummyDialogComponent, DropdownComponent],
