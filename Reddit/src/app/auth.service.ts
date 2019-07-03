2c0e99be0f8190ffc5912044df3be5691d0ea928
Soma <soma.feigl@gmail.com>
2019-07-03 15:44:07 +0200

    added token functions

--- Reddit/src/app/auth.service.ts
+++ Reddit/src/app/auth.service.ts
@@ -1,40 +1,113 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Subject } from 'rxjs';
-// const jwt = require('jsonwebtoken'); //FIXME
+import { of, Observable, BehaviorSubject } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators';
+import { config } from './config';
+import { Tokens } from './token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  public username: BehaviorSubject<any> = new BehaviorSubject(1);
-  obs = this.username.asObservable();
-  public checker: Subject<boolean> = new Subject;
-  obs2 = this.checker.asObservable();
+  checker: any;
 
-  constructor(private http: HttpClient) {
-  }
-
-  get user(): BehaviorSubject<string> {
-    return this.username
-  }
 
   sendData(username: string, password: string) {
     let headers: HttpHeaders = new HttpHeaders();
     headers = headers.append('Content-Type', 'application/json');
-    // let token = jwt.sign(payload, 'secretKey'); //FIXME
-    // headers = headers.append('Authorization', '');
     let payload = {
       username: username,
       password: password
     };
-    this.http.post('http://localhost:3000/login', payload, { headers: headers }).subscribe(
+    this.http.post('http://localhost:3200/login', payload, { headers: headers }).subscribe(
       res => {
-        this.username.next({ succes: true, username });
-        this.checker.next(true) 
+        console.log('apa');
+        this.checker.next(true)
       },
       err => {
-        this.checker.next(false) 
+        console.log('anya');
+
+        this.checker.next(false)
       })
+  };
+
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  public loggedUser: BehaviorSubject<string> = new BehaviorSubject('');
+  obs = this.loggedUser.asObservable();
+
+  get user(): BehaviorSubject<string> {
+    return this.loggedUser
   }
-};
+
+  constructor(private http: HttpClient) { }
+
+  login(user: { username: string, password: string }): Observable<boolean> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    return this.http.post<any>(`${config.apiUrl}/login`, user, { headers: headers })
+      .pipe(
+        tap(res => {this.doLoginUser(user.username, res.tokens)}),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        }));
+  }
+
+  logout() {
+    return this.http.post<any>(`${config.apiUrl}/logout`, {
+      'refreshToken': this.getRefreshToken()
+    }).pipe(
+      tap(() => this.doLogoutUser()),
+      mapTo(true),
+      catchError(error => {
+        alert(error.error);
+        return of(false);
+      }));
+  }
+
+  isLoggedIn() {
+    return !!this.getJwtToken();
+  }
+
+  refreshToken() {
+    return this.http.post<any>(`${config.apiUrl}/refresh`, {
+      'refreshToken': this.getRefreshToken()
+    }).pipe(tap((tokens: Tokens) => {
+      this.storeJwtToken(tokens.jwt);
+    }));
+  }
+
+  getJwtToken() {
+    return localStorage.getItem(this.JWT_TOKEN);
+  }
+
+  private doLoginUser(username: string, tokens: Tokens) {
+    this.loggedUser.next(username);
+    this.storeTokens(tokens);
+  }
+
+  private doLogoutUser() {
+    this.loggedUser.next('');
+    this.removeTokens();
+  }
+
+  private getRefreshToken() {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
+
+  private storeTokens(tokens: Tokens) {
+    localStorage.setItem(this.JWT_TOKEN, tokens.jwt);
+    localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
+}
