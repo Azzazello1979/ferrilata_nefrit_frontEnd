31f070809371890b3a2557ae0a977e4346298934
Azzazello1979 <balint.haui@gmail.com>
2019-07-05 16:36:14 +0200

    typos

--- Reddit/src/app/services/expired-token-interceptor.service.ts
+++ Reddit/src/app/services/expired-token-interceptor.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { AuthorizationService } from './authorization.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, filter, take, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +10,15 @@ import { catchError } from 'rxjs/operators';
 
 export class ExpiredTokenInterceptorService implements HttpInterceptor {
 
+  private isRefreshingInProgress = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
   constructor(
     private _authorizationService: AuthorizationService
   ) { }
 
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     // if accessToken is present in local storage...
     if (this._authorizationService.getAccessToken()) {
@@ -26,13 +29,15 @@ export class ExpiredTokenInterceptorService implements HttpInterceptor {
     // if accessToken not present in local storage...
     // use catchError and listen for http errors...
     return next.handle(request)
-    .pipe(catchError(error => {
-      if (error instanceof HttpErrorResponse && error.status === 401 && error.message === 'Expired token.') {
-        return this.handle401Error(request, next);
-      } else {
-        return throwError(error);
+      .pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse && error.status === 401 && error.message === 'Expired token.') {
+          return this.handle401Error(request, next);
+        } else {
+          return throwError(error);
+        }
       }
-    }));
+      )
+      );
 
   }
 
@@ -47,8 +52,43 @@ export class ExpiredTokenInterceptorService implements HttpInterceptor {
 
 
 
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshingInProgress) {
+      this.isRefreshingInProgress = true;
+      this.refreshTokenSubject.next(null);
+
+      return this._authorizationService.doRefreshToken().pipe(
+        switchMap((token: any) => {
+          this.isRefreshingInProgress = false;
+          this.refreshTokenSubject.next(token.jwt);
+          return next.handle(this.addToken(request, token.jwt));
+        }));
+
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(token => token != null),
+        take(1),
+        switchMap(jwt => {
+          return next.handle(this.addToken(request, jwt));
+        }));
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
 
 
 
