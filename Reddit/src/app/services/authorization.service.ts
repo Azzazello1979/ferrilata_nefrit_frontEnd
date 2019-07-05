f2f0948ba79c5e2bca2ab22a742f0ff0bdb17b5b
Azzazello1979 <balint.haui@gmail.com>
2019-07-05 16:35:47 +0200

    typos

--- Reddit/src/app/services/authorization.service.ts
+++ Reddit/src/app/services/authorization.service.ts
@@ -1,18 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators';
+
+import { Tokens } from '../models/tokens';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
 
-  private readonly accessToken = 'accessToken';
 
-  constructor() { }
+
+  API_URL = 'http://localhost:4200'; //move to enviroment variable!
+
+  private readonly accessToken = 'accessToken';
+  private readonly refreshToken = 'refreshToken';
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
 
   getAccessToken() {
     return localStorage.getItem(this.accessToken);
   }
+
+  getRefreshToken() {
+    return localStorage.getItem(this.refreshToken);
+  }
+
+
+  storeAccessToken(jwt: string) {
+    localStorage.setItem(this.accessToken, jwt);
+  }
+
+
+  storeTokens(tokens: Tokens) {
+    localStorage.setItem(this.accessToken, tokens.jwt);
+    localStorage.setItem(this.refreshToken, tokens.refreshToken);
+  }
+
+
+
+ // send locally stored refreshToken to /refresh endpoint
+  doRefreshToken() {
+    return this.http.post<any>(`${this.API_URL}/refresh`, {
+      'refreshToken': this.getRefreshToken()
+    })
+      .pipe(
+        tap((tokens: Tokens) => {
+          this.storeAccessToken(tokens.jwt);
+        }
+        )
+      );
+  }
+
+
+
 
 
 }
