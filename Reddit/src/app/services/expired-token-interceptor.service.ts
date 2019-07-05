e2727780ef6d8520a71c14b4aee206ef1a7de9c7
Azzazello1979 <balint.haui@gmail.com>
2019-07-05 14:39:57 +0200

    HttpInterceptor interface

---
+++ Reddit/src/app/services/expired-token-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpiredTokenInterceptorService implements HttpInterceptor{
+
+  constructor() { }
+
+
+  intercept(){
+
+  }
+
+
+  
+
+}
