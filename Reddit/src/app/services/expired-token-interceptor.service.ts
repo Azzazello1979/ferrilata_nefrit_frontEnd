7f1eaad9d85f36d1a212a9a26db67411264e230b
Azzazello1979 <balint.haui@gmail.com>
2019-07-07 18:00:34 +0200

    more comments

--- Reddit/src/app/services/expired-token-interceptor.service.ts
+++ Reddit/src/app/services/expired-token-interceptor.service.ts
@@ -24,19 +24,24 @@ export class ExpiredTokenInterceptorService implements HttpInterceptor {
     if (this._authorizationService.getAccessToken()) {
       request = this.addToken(request, this._authorizationService.getAccessToken());
       // now, the captured request will be transformed by addToken() method and will contain the
-      // token in the header of the request: Authorization: Bearer xx.yy.zz
+      // access token in the header of the request: Authorization: Bearer xx.yy.zz
     }
-    // if accessToken not present in local storage...
+
+
+    // Error handling for 401 - unauthorized (access token changed)
     // use catchError and listen for http errors...
+    // handle modified request to next interceptor - or backend
+    // if error is HttpErrorResponse of 401, try to refresh token
     return next.handle(request)
-      .pipe(catchError(error => {
-        if (error instanceof HttpErrorResponse && error.status === 401 && error.message === 'Expired token.') {
-          return this.handle401Error(request, next);
-        } else {
-          return throwError(error);
+      .pipe(
+        catchError(error => {
+          if (error instanceof HttpErrorResponse && error.status === 401 && error.message === 'Expired token.') {
+            return this.refreshAfter401(request, next);
+          } else {
+            return throwError(error);
+          }
         }
-      }
-      )
+        )
       );
 
   }
@@ -52,18 +57,35 @@ export class ExpiredTokenInterceptorService implements HttpInterceptor {
 
 
 
-  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
-    if (!this.isRefreshingInProgress) {
+  private refreshAfter401(request: HttpRequest<any>, next: HttpHandler) {
+    // execute token refreshing
+    if (!this.isRefreshingInProgress) { // entering if isRefreshingInProgress = false
       this.isRefreshingInProgress = true;
       this.refreshTokenSubject.next(null);
+      // the next emit from this subject will be null
+      // this will block another request in the second part
 
-      return this._authorizationService.doRefreshToken().pipe(
-        switchMap((token: any) => {
-          this.isRefreshingInProgress = false;
-          this.refreshTokenSubject.next(token.jwt);
-          return next.handle(this.addToken(request, token.jwt));
-        }));
 
+      // calling doRefreshToken() to get a new access token
+      return this._authorizationService.doRefreshToken()
+        .pipe(
+          switchMap((token: any) => {
+            // Rxjs operator switchMap()... used on observables
+            // switches observables ... cancels that observable that started
+            // longer time ago in favor of the fresh observable ...
+            // cancel old observable so you dont have to deal with the data that 
+            // will eventually come back ... cancel http requests
+            // emit the values of the fresh observable
+            this.isRefreshingInProgress = false;
+            // once we have new access token, we can indicate that refreshing finished...
+            this.refreshTokenSubject.next(token.jwt);
+            // ... and set refreshTokenSubject next emit to the fresh access token
+            return next.handle(this.addToken(request, token.jwt));
+          }));
+
+      // blocking & releaseing all other queries that started during 
+      // the refreshing process, which we want to put on hold until we have new 
+      // access token
     } else {
       return this.refreshTokenSubject.pipe(
         filter(token => token != null),
@@ -73,23 +95,4 @@ export class ExpiredTokenInterceptorService implements HttpInterceptor {
         }));
     }
   }
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
