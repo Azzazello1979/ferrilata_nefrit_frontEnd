6f1c661695806340a6d80d973dfac2efab890223
Soma <soma.feigl@gmail.com>
2019-07-09 10:02:27 +0200

    added guard && modified routing-module

--- Reddit/src/app/app-routing.module.ts
+++ Reddit/src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth.guard';
 import { PostsComponent } from './components/posts/posts.component';
-import { DummyDialogComponent } from './components/dummy-dialog/dummy-dialog.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    canActivate: [AuthGuard]
   },
   {
     path: '',
     component: PostsComponent
+  }
+  ,{
+    path: '/submit',
+    component: SubmitComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
