3bc9ff333b3d56d43f82c299c593f27d9448aa5b
Ruszin Peter <ruszin.peter@gmail.com>
2019-07-09 16:18:06 +0200

    add received channels into dropdown menu

--- Reddit/src/app/components/dropdown/dropdown.component.ts
+++ Reddit/src/app/components/dropdown/dropdown.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 import { MatSelectChange } from "@angular/material";
 import { ChannelService } from "src/app/services/channel.service";
 
@@ -22,20 +26,20 @@ export class DropdownComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    const receivedChannels = this.channelsvc.getChannels();
+    receivedChannels.subscribe((channelsvcData: any) => {
+      this.channels = channelsvcData;
+      console.log(this.channels);
+      this.entities = this.channels;
+    });
     this.entity = this.formBuilder.group({
       entity: [null, Validators.required]
     });
-    this.entity.get('entity').setValue(event);
+    this.entity.get('entity').setValue(this.defaultValue);
   }
 
   outputEntity(event: any) {
     this.selectionChange.emit(event);
   }
 
-  getChannels() {
-    const receivedChannels = this.channelsvc.getChannels();
-    receivedChannels.subscribe((channelsvcData:[]) => {
-      this.channels = channelsvcData;
-    });
-  }
 }
